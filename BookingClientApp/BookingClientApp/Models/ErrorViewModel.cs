5e417f5ce9346d8e37dd85a56688a7958593a9ec
Dominator4 <103027092+Dominator4@users.noreply.github.com>
2024-03-04 07:37:13 +0100

    Dodaj pliki projektów.

---
+++ BookingClientApp/BookingClientApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookingClientApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
